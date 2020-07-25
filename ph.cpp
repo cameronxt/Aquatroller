9f73a9ceb41893eccda471bba8f9ada9cbcb404e
Cameronxt <cameronxt@gmail.com>
2020-07-25 11:56:11 -0500

    Basic Setters/Getters for PH

--- ph.cpp
+++ ph.cpp
@@ -1,3 +1,4 @@
+
 #include "ph.h"
 
 PH::PH() {
@@ -5,34 +6,91 @@ PH::PH() {
 }
 
 void PH::init() {
-  
+  // Set initial variables if we find them stored in eeprom
 }
 
 void PH::loop(unsigned long currentTime){
-  if (prevPhTime - currentTime >= checkPhDelay) {
+
+  // Timer so we only read ph every so often
+  if (phData.prevPhTime - currentTime >= phData.checkPhDelay) {
     //Serial.println(F("its time to check it"));
-    buf[phIndex]=analogRead(pin);               // store anolg value into buffer position
-    currentTime = millis();                        // Reset timer to allow ph sensor to stabilize
-    if (phIndex < bufSize){
-      phIndex++;
-    } else {
-      for(int i=0;i<(bufSize-1);i++) {
-        for(int j=i+1;j<bufSize;j++) {
-          if(buf[i]>buf[j]) {
-            temp=buf[i];
-            buf[i]=buf[j];
-            buf[j]=temp;
+    phData.buf[phData.phIndex]=analogRead(_phPin);               // store anolg value into buffer position
+    phData.prevPhTime = millis();                         // Reset timer to allow ph sensor to stabilize before next reading
+    if (phData.phIndex < _bufSize){                       // if not full advance to the next index
+      phData.phIndex++;
+    } else {                                        // If full calculate average value
+      for(int i=0;i<(_bufSize-1);i++) {             // First lets sort our results
+        for(int j=i+1;j<_bufSize;j++) {
+          if(phData.buf[i]>phData.buf[j]) {
+            phData.temp=phData.buf[i];
+            phData.buf[i]=phData.buf[j];
+            phData.buf[j]=phData.temp;
           }
         }
       }
-       avgValue=0;
-       for(int i=2;i<8;i++) avgValue+=buf[i];
-       float pHVol=(float)avgValue*5.0/1024/6;
-       float phValue = -5.70 * pHVol + 21.34;
+      
+       phData.avgValue=0;                                         // Reset average value       
+       for(int i=1;i<_bufSize-1;i++) phData.avgValue+=phData.buf[i];      // Drop highest and lowest value, add the rest together
+       
+       float pHVol=(float)phData.avgValue*5.0/1024/8;              // average and convert to millivolts
+       float phValue = -5.70 * pHVol + 21.34;               // convert millivolts to PH reading
+       
        Serial.print("PH = ");
        Serial.println(phValue);
-       currentTime = millis();
-       phIndex = 0;
+      
+       phData.prevPhTime = millis();                              // Reset the timer and buffer index
+       phData.phIndex = 0;
     }
+  }
+}
+
+
+
+void PH::setTargetPH(float newTarget) {
+  phData.targetPh = newTarget;
+}
+
+float PH::getTargetPH() {
+  return phData.targetPh;
+}
+
+void PH::setPHDelay(unsigned long newDelay) {
+  phData.checkPhDelay = newDelay;
+}
+
+// Returns the delay time between PH Checks
+unsigned long PH::getPHDelay() {
+  return phData.checkPhDelay;
+}
+
+
+// Returns the most recent PH reading
+float PH::getCurrentPH() {
+  return phData.currentPh;
+}
+
+// Enter probe calibration mode... 
+// TODO: LOTS!!!!
+void PH::calibratePH() {
+     Serial.println(F("PH Calibration Mode"));
+
+     
+     Serial.println(F("Enter Calibration Target 1"));
+     Serial.println(F("Waiting to stabilize..."));
+     // Timer goes here
+     Serial.println(F("Reading Actual 1"));
+
+     
+     Serial.println(F("Enter Calibration Target 2"));
+     Serial.println(F("Waiting to stabilize..."));
+     // Timer goes here
+     Serial.println(F("Reading Actual 2"));
+
+     setCalPoints(phData.calActual);
+}
+
+void PH::setCalPoints(float calActual[]) {
+  for (byte i = 0; i < 2; i++) {
+    phData.calActual[i] = calActual[i];
   }
 }
--- ph.h
+++ ph.h
@@ -1,29 +1,51 @@
 #ifndef ph_h
 #define ph_h
 #include <Arduino.h> 
-const int pin = A0;   // PH Sensor Analog pin
-const int bufSize = 10;
 
-class PH {
+const int _phPin = A0;   // PH Sensor Analog pin
+const int _bufSize = 10;
+
+struct PHData  {
     int sensorValue = 0;  // Direct Sensor Value (0-1024)
     unsigned long avgValue; // Calculated average
     unsigned long checkPhDelay = 1000;
     unsigned long prevPhTime = 0;
-    unsigned long currentTime;
-    int buf[bufSize], temp;          // buffer to average results
-    int phIndex = 0;
+ //   unsigned long _currentTime;
+    int buf[_bufSize], temp;          // buffer to average results
+    int phIndex = 0;                  // Index for PH buffer
+    float targetPh = 7.0;
+    float currentPh;
+
+    float calTarget[2];
+    float calActual[2];
+    bool calibrationMode = false;
+};
+
+class PH {
+   
+    PHData phData;
+  
+
     
   public:
+
+
     PH();
-    void init();
-    void loop(unsigned long CurrentTime);
-    void setTargetPH(float newPH);
-    float getTargetPH();
-    void setPHDelay(unsigned long NewDelay);
-    unsigned long getPHDelay();
-    void getCurrentPH();
-    void calibrate();             
+      void init();
+      void loop(unsigned long currentTime);
+      void setTargetPH(float newTarget);
+      float getTargetPH();
+      void setPHDelay(unsigned long newDelay);
+      unsigned long getPHDelay();
+      float getCurrentPH();
+      void calibratePH();      
+  
+     // Variables
+
+   
   private:
+    void setCalPoints(float calActual[]);
+
 };
 
 #endif
