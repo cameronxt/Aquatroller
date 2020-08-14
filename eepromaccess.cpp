92bd9a5cb8eaaac5edbcd5da18687fda01df3d71
Cameronxt <cameronxt@gmail.com>
2020-08-14 18:29:01 -0500

    Corrected EEPROM reading and recieving

--- eepromaccess.cpp
+++ eepromaccess.cpp
@@ -1,14 +1,14 @@
 //read eeprom as a structure
 //write to eeprom as a structure
 #include "eepromaccess.h"
-EepromAccess::EepromAccess(PHData *phData, LightData *lightData): _phData(phData), _lightData(lightData)
+EepromAccess::EepromAccess(PHData *phData, LightData *lightData, TemperatureData *tempData) : _phData(phData), _lightData(lightData), _tempData(tempData)
 {}
 
-void EepromAccess::saveMap() {         // Write Settings to EEPROM
+void EepromAccess::saveMap() {    // Write Settings to EEPROM
   EEPROM.put(settingsAddressEEPROM, _eepromMap);
 }
 
-EMap EepromAccess::getMap() {       // Read Settings From EEPROM
+EMap EepromAccess::getMap() {   // Read Settings From EEPROM
   EMap eMap;
   EEPROM.get(settingsAddressEEPROM, eMap);
   return eMap;
@@ -17,32 +17,30 @@ EMap EepromAccess::getMap() {       // Read Settings From EEPROM
 
 bool EepromAccess::checkForSettings (EMap checkMap) {
 
-  if (strstr(checkMap.VERSION, _eepromMap.VERSION)) {         // If previous version is found, then true
+  if (strstr(checkMap.VERSION, _eepromMap.VERSION)) {   // If previous version is found, then true
     Serial.println(F("Previous config found..."));
     return true;
-  } else {                                        // else false
+  } else {                                      // else false
     Serial.println(F("No config found..."));
     return false;
   }
 }
 
 // TODO: Check for settings in eeprom and load into memory if there
-void EepromAccess::setup() {                      // Check for previous config, if not found generate new one
+void EepromAccess::setup() {    // Check for previous config, if not found generate new one
 
-  EMap tempMap = getMap();;                                      // New Temporary EEPROM map
-  //getMap();                                  // Read settings from eeprom
+  EMap tempMap = getMap();      // Put eeprom contents in new Temporary EEPROM map
+  //getMap();                   // Read settings from eeprom
 
   if (checkForSettings(tempMap))  {                      // If the map from EEPROM contains previous data
-    Serial.println(F("Loading Settings from EEPROM"));
-    //_eepromMap = getMap();                                  // Load it
+    Serial.print(F("Loading Settings from EEPROM..."));
+    getSettings();                                       // Load it
+    Serial.println(F(" Done"));
 
-    getSettings();
-
-    //getSettings(*_lightData);      // Read Settings From EEPROM
-
-  } else {                                                // Otherwise, config as defaults
+  } else {                                        // Otherwise, config as defaults
     Serial.print(F("Generating new config..."));
-    _eepromMap = EMap();                                   // Create default structure
+    resetEeprom();                                // Reset EEPROM so we dont have any weird values
+    _eepromMap = EMap();                          // Create default structure
     updateSettings();                             // Sets everything to default values
     Serial.println(F(" Done"));
   }
@@ -55,7 +53,7 @@ void EepromAccess::updateSettings() {           // Updates(not write) eeprom wit
   EEPROM.put(lightAddressEEPROM, _lightData);
 }
 
-void EepromAccess::getSettings() {
+void EepromAccess::getSettings() {    // Get all settings from EEPROM
 
   EEPROM.get(settingsAddressEEPROM, _eepromMap);
   EEPROM.get(phAddressEEPROM, *_phData );
@@ -64,15 +62,26 @@ void EepromAccess::getSettings() {
 
 void EepromAccess::loop() {
   // Timer to reduce writes to eeprom
-  // compare eeprom to current values
-  if (_needsUpdated) {
-    updateSettings();
-    _needsUpdated = false;
+  if (millis() - _prevEepromTime >= _eepromMap.checkEepromDelay) {
+    // compare eeprom to current values
+    if (_needsUpdated) {        // Flagged by an incoming command that changes eeprom value
+      updateSettings();
+      _needsUpdated = false;    // Reset the flag
+    }
+    _prevEepromTime = millis();
   }
 
 }
 
 
+// Reset EEPROM by writing 0's to every cell
+void EepromAccess::resetEeprom() {
+  Serial.println(F("Clearing EEPROM..."));
+  for (int i = 0; i < 1024; i++) {
+    EEPROM.put(i, 0);
+  }
+  Serial.println(F("EEPROM Cleared"));
+}
 void EepromAccess::updateSelectEeprom(EEPROM_SELECT selection) {
   switch (selection) {
     case ALL:
--- eepromaccess.h
+++ eepromaccess.h
@@ -1,16 +1,17 @@
 /*
- * EEPROM Access by Cameron Tibbits
- * 
- * Library for writing structs to the internal eeprom
- * all other classes will pass a pointer to their data
- * strutures. This class hast a setup()function that 
- * will check the eeprom for previous Data with the correct
- * version number. If we find one then those values will 
- * be loaded into ram. If there is no previous config it 
- * will generate a new map with default value. Default values
- * are determined by their parent class.
- * 
- */
+   EEPROM Access by Cameron Tibbits
+
+   Library for writing structs to the internal eeprom.
+   All other classes will pass a pointer to their data
+   strutures. This class hast a setup()function that
+   will check the eeprom for previous Data with the correct
+   version number. If we find one then those values will
+   be loaded into ram. If there is no previous config it
+   will generate a new map with default values. Default values
+   are determined by their parent class. Also allows saving 
+   of individual structs or all at once
+
+*/
 
 #ifndef eeproma_h
 #define eeproma_h
@@ -18,9 +19,10 @@
 #include <EEPROM.h>
 #include "ph.h"
 #include "lights.h"
+#include "temp.h"
 
 
-  
+
 enum EEPROM_SELECT {
   NONE,
   ALL,
@@ -32,7 +34,7 @@ enum EEPROM_SELECT {
 struct EMap {           // Struct to store all of eeprom settings
 
   char VERSION[4] = "AA3";     // number to check and see if storage is valid
-  int checkVar = 1;
+  unsigned long checkEepromDelay = 30*1000;
 
   // Partition Map: holds start address of each stored struct in EEPROM
 
@@ -41,11 +43,12 @@ struct EMap {           // Struct to store all of eeprom settings
 const int settingsAddressEEPROM = 0;
 const int phAddressEEPROM = sizeof(EMap) + 1;                  // Address offset for PH data
 const int lightAddressEEPROM = sizeof(PHData) + phAddressEEPROM + 1;   // Address offset for Light data
+const int tempAddressEEPROM = sizeof(LightData) + lightAddressEEPROM + 1;
 
 class EepromAccess {
 
   public:
-  EepromAccess(PHData *, LightData *);
+    EepromAccess(PHData *, LightData *, TemperatureData *);
     void setup();
     void loop();
     // select settings, set and get
@@ -54,19 +57,23 @@ class EepromAccess {
     // All settings, set and get
     void updateSettings();
     void getSettings();
+    void resetEeprom();
   private:
     bool checkForSettings (EMap checkMap);
     bool settingsChanged;
     void saveMap();
     EMap getMap ();
-    
+
     bool _needsUpdated = false;   // Flag for when to write new data
-    
+    unsigned long _prevEepromTime;
+
 
 
   public:  EMap _eepromMap;
-    EEPROM_SELECT _settingSelect; // Which Data to get or retrieve
+    enum EEPROM_SELECT _settingSelect; // Which Data to get or retrieve
     PHData* _phData;              // Pointer to PH Data to be stored
     LightData* _lightData;        // Pointer to Light Data to be stored
+    TemperatureData* _tempData;   // Pointer to temperature data to be stored
+    
 };
 #endif
